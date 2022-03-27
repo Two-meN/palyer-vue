cb74912d6a11fcad361f560c949c49479e77e53d
Anatoly <avm.market@yandex.ru>
2022-03-27 14:05:00 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="wrapper">
+    <nav>
+      <RouterLink to="/">HomeView</RouterLink>
+    </nav>
+  </div>
+  <RouterView/>
+</template>
+
---
+++ src/components/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <button>Primary</button>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+export default class HomeView extends Vue {
+
+}
+</script>
